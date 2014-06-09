171740ddef8f4d17accc339a109620508aaa1f42
marindraganov <marodraganov@gmail.com>
2014-06-10 01:02:31 +0300

    Another try to merge

--- scripts/game.js
+++ scripts/game.js
@@ -1,17 +1,17 @@
 ﻿var fieldWalls = [
-    "----------------------- ",
-    "||  |          |       |",
-    "-- -           --     - ",
-    "|  |           |  |    |",
-    "--     --     -----    |",
-    "|  |   |       |       |",
-    "- -            -- --    ",
-    "|      |           |   |",
-    "    ----                ",
-    "|  | | |           |   |",
-    "   -           -----    ",
-    "|   |  |           |   |",
-    "----------------------- "
+    "---------------------- ",
+    "||  |          |      |",
+    "-- -           --    - ",
+    "|  |           |  |   |",
+    "--     --     -----   |",
+    "|  |   |       |      |",
+    "- -            -- --   ",
+    "|      |           |  |",
+    "    ----               ",
+    "|  | | |           |  |",
+    "   -           -----   ",
+    "|   |  |           |  |",
+    "---------------------- "
 ];
 
 var cellHeight = 50;
@@ -196,15 +196,131 @@ function StartChangeDirectionListener(pacMan) {
     }
 }
 
+function Guardian(x, y, radius, speed, direction, fillColor, strokeColor) {
+
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.direction = direction;
+	this.radius = radius;
+	this.fillColor = fillColor;
+	this.strokeColor = strokeColor;
+
+
+
+	this.draw = function(ctx) {
+		ctx.beginPath();
+		ctx.quadraticCurveTo(this.x - this.radius, this.y + this.radius, this.x, this.y);
+		ctx.lineTo(this.x + this.radius, this.y + this.radius);
+		ctx.arc(this.x, this.y, this.radius, Math.PI, 2 * Math.PI);
+		ctx.closePath();
+		ctx.fillStyle = this.fillColor;
+		ctx.strokeStyle = this.strokeColor;
+		ctx.stroke();
+    	ctx.fill();
+	 };
+
+
+
+    this.move = function (direction) {
+
+		switch (this.direction) {
+			case 'up':
+				this.y += -this.speed;
+				break;
+			case 'down':
+				this.y += this.speed;
+				break
+			case 'left':
+				this.x += -this.speed;
+				break;
+			case 'right':
+				this.x += this.speed;
+				break;
+		}
+    }
+}
+
+function creatGuardians(guardiansCount) {
+    guardians = [];
+
+    for (i = 0; i < guardiansCount; i++) {
+    	var x = getRandomValue(10, 780),			//TODO
+    		y = getRandomValue(10, 180 ),           //TODO
+    		radius = 7,
+    		direction = randomDirection(),
+    		fillColor = getRandomColor();
+    		ctx.lineWidth = 3;
+    				
+    	var guardian = new Guardian(x, y, radius, 3, direction, fillColor, 'yellow');
+    
+    	guardians.push(guardian);
+    }
+
+    return guardians;
+}
+
 drawField(fieldWalls);
+
+var canvas = document.getElementById("canvas"),
+ctx = canvas.getContext("2d"),
+guardians = creatGuardians(4);
+
 var pacMan = new PacMan(128,28, 'left');
 StartChangeDirectionListener(pacMan);
 
 function gameCicle()
 {
-    pacMan.draw();
-    pacMan.move();
-    console.log();
+     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);	//clear
+     pacMan.draw();
+     pacMan.move();
+
+    for (i = 0; i < guardians.length; i++) {
+		guardians[i].draw(ctx);
+		guardians[i].move();
+
+	}
 }
 
 setInterval(function () {gameCicle();}, 40);
+
+
+//random functions
+
+function getRandomValue(min, max) {
+
+	return (Math.random() * (max - min) + min) | 0;
+
+}
+
+function getRandomColor() {
+		var red = getRandomValue(0, 255);
+		var blue = getRandomValue(0, 255);
+		var green = getRandomValue(0, 255);
+
+		return "rgb(" + red + "," + green + "," + blue + ")";
+}
+
+function randomDirection(direction) {
+
+	if (Math.random() <= 0.25){
+
+		direction = "left";
+
+	} else if (Math.random() > 0.25 && Math.random() <= 0.5) {
+
+		direction = "right";
+
+	} else if (Math.random() > 0.5 && Math.random() <= 0.75) {
+
+		direction = "up";
+
+	} else {
+
+		direction = "down";
+
+	}
+
+return direction;		
+
+}
